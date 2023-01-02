3d40234bd206b5eb90a20e7b9c55b9a0d392a66d
Jaeyoon222 <sbjaeyoon222@gmail.com>
2023-01-03 07:49:51 +0900

    Login : Events 2

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector(".loginForm");
 const loginInput = document.querySelector(".loginForm input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(event);
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
