799ae77057169ad047a269a204bad281aa618b8e
Jaeyoon222 <sbjaeyoon222@gmail.com>
2023-01-03 07:22:40 +0900

    Login : Form submission 1

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector(".loginForm");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+
 function onLoginBtnClick() {
-    console.log("click!");
-    console.log("loginInput value: " + loginInput.value);
+    const userId = loginInput.value;
+
+    if (userId === "") {
+        return console.log("Please write ID");
+    }
+    if (userId.length > 15) {
+        return console.log("ID is too long");
+    }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
