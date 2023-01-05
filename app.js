36d6f818625c3a2e2077c134f36d38db524ec64a
Jaeyoon222 <sbjaeyoon222@gmail.com>
2023-01-05 09:43:49 +0900

    Login : Loading  UserId

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const loginInput = document.querySelector(".loginForm input");
 const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const ID_KEY = "id";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     const userId = loginInput.value;
-    console.log(userId);
-    localStorage.setItem("userId", userId);
+    localStorage.setItem(ID_KEY, userId);
+    paintGreeting(userId);
+}
+
+function paintGreeting(userId) {
     greeting.innerText = `Welcome ${userId}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedId = localStorage.getItem(ID_KEY);
+
+if (savedId === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+if (savedId !== null) {
+    paintGreeting(savedId);
+}
